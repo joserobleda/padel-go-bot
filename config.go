5dd2d8b6a27badbe61800794a302626ef63dffb0
Jose Ignacio <joserobleda@gmail.com>
2017-11-09 21:26:21 +0100

    adjust keyboard responses

--- config.go
+++ config.go
@@ -27,8 +27,8 @@ func ConfigDateHandler(message *tbot.Message) {
     {"/at 18:00", "/at 18:30"},
   }
 
-  msg := "Cool! I'll will reserve for " + dayOfWeek + "s. Do you want to play at "+ hourToPlay +"?";
-  message.ReplyKeyboard(msg, buttons, tbot.OneTimeKeyboard)
+  msg := "Cool! I'll will reserve for " + dayOfWeek + "s. *Do you want to play at "+ hourToPlay +"?*";
+  message.ReplyKeyboard(msg, buttons, tbot.OneTimeKeyboard, tbot.WithMarkdown)
 }
 
 func ConfigTimeHandler(message *tbot.Message) {
@@ -61,9 +61,9 @@ func ConfigShowHandler(message *tbot.Message) {
   tdate := ndate.AddDate(0, 0, -7);
 
   msgs := []string {
-    "I'll will reserve for "+ dayOfWeek +"s at "+ hourToPlay +". Next date available for schedule is "+ ndate.Format("02-01-2006") +".",
-    "And don't worry, I'll automatically perform the reservation on "+ tdate.Format("02-01-2006") +"'s night",
+    "I'll will reserve for *"+ dayOfWeek +"s at "+ hourToPlay +"*. Next available date for schedule is *"+ ndate.Format("02-01-2006") +"*.",
+    "_And don't worry, I'll automatically perform the reservation on "+ tdate.Format("02-01-2006") +"'s night_",
   }
 
-  message.Reply(strings.Join(msgs, "\n"))
+  message.Reply(strings.Join(msgs, "\n"), tbot.WithMarkdown)
 }
