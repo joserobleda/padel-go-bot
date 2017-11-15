be6570e2cf5a14c63df3e4ef2e2c404600f98ec1
Jose Ignacio <joserobleda@gmail.com>
2017-11-15 22:45:56 +0100

    fix auto-reminder

--- auto-reminder.go
+++ auto-reminder.go
@@ -1,46 +1,47 @@
 package main
 
 import (
-	"github.com/yanzay/tbot"
-	"time"
 	"fmt"
+	"time"
+
+	"github.com/yanzay/tbot"
 )
 
 func autoReminder(server *tbot.Server) {
-	fmt.Println("Auto reminder enabled");
+	fmt.Println("Auto reminder enabled")
 
-  location, _ := time.LoadLocation("Europe/Madrid")
+	location, _ := time.LoadLocation("Europe/Madrid")
 
 	for date := range time.Tick(time.Minute) {
-		date = date.In(location);
-		currentTime := date.Format("15:04:05")
+		date = date.In(location)
+		currentTime := date.Format("15:04")
 
-		if currentTime != "20:00:00" {
+		if currentTime != "22:45" {
 			continue
 		}
 
-		err, rsvp := getLatestActiveReservation();
+		err, rsvp := getLatestActiveReservation()
 
 		if err != nil {
 			fmt.Println(err)
-			break;
+			break
 		}
 
 		remainingTime := time.Until(rsvp.date)
-		remainingHours := remainingTime.Hours();
+		remainingHours := remainingTime.Hours()
 
 		if remainingHours > 24 {
-			continue;
+			continue
 		}
 
-		fmt.Println("Remaining time: " + remainingTime.String());
+		fmt.Println("Remaining time: " + remainingTime.String())
 
 		if chatId != 0 {
 			msg := "Eeeey!!\n"
-			msg += "There is a match tomorrow at "+ rsvp.date.Format("15:04") +"! "
+			msg += "There is a match tomorrow at " + rsvp.date.Format("15:04") + "! "
 			msg += "Don't forget to get your bag!!"
 
-			server.Send(chatId, msg);
+			server.Send(chatId, msg)
 		}
 	}
 }
