048145448cdc73ef5c094eb65f0c3ec75196e914
Jose Ignacio <joserobleda@gmail.com>
2017-11-15 22:46:30 +0100

    fix reserve lint

--- reserve.go
+++ reserve.go
@@ -1,39 +1,39 @@
 package main
 
 import (
-  "strings"
-  "github.com/headzoo/surf/browser"
-  "fmt"
+	"fmt"
+	"strings"
+
+	"github.com/headzoo/surf/browser"
 )
 
 func reserve(bro *browser.Browser, date string) (string, int) {
-  name, resource := checkDate(bro, date)
+	name, resource := checkDate(bro, date)
 
-  if resource == "" {
-    return "Oh! Looks like there are no available tracks! Sorry!!", 1;
-  }
-  // fmt.Println(bro.Body());
+	if resource == "" {
+		return "Oh! Looks like there are no available tracks! Sorry!!", 1
+	}
 
-  rsrvefm, _ := bro.Form("form#newreservation")
-  rsrvefm.Input("duration", "90")
-  if rsrvefm.Submit() != nil {
-     panic("form#newreservation error")
-  }
+	rsrvefm, _ := bro.Form("form#newreservation")
+	rsrvefm.Input("duration", "90")
+	if rsrvefm.Submit() != nil {
+		panic("form#newreservation error")
+	}
 
-  paymentfm, err := bro.Form("form#paymentForm")
-  if (err != nil) {
-    fmt.Println(bro.Body());
-    return err.Error(), 2;
-  }
+	paymentfm, err := bro.Form("form#paymentForm")
+	if err != nil {
+		fmt.Println(bro.Body())
+		return err.Error(), 2
+	}
 
-  paymentfm.Input("idPaymentMethod", "968")
-  if paymentfm.Submit() != nil {
-     panic("form#paymentForm error")
-  }
+	paymentfm.Input("idPaymentMethod", "968")
+	if paymentfm.Submit() != nil {
+		panic("form#paymentForm error")
+	}
 
-  if strings.TrimSpace(bro.Title()) == "Reserva Confirmada" {
-    return "Track " + name + " reserved!", 0;
-  }
-  
-  return "I can't reserve! Maybe you need to put more money? Sorry!!", 3;
+	if strings.TrimSpace(bro.Title()) == "Reserva Confirmada" {
+		return "Track " + name + " reserved!", 0
+	}
+
+	return "I can't reserve! Maybe you need to put more money? Sorry!!", 3
 }
