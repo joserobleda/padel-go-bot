b1619945c14bfe4cab07d53a379d0b2d59c5dabc
Jose Ignacio <joserobleda@gmail.com>
2017-11-21 22:18:09 +0100

    fix file

--- config.go
+++ config.go
@@ -2,68 +2,69 @@ package main
 
 import (
 	"strings"
-  "github.com/yanzay/tbot"
+
+	"github.com/yanzay/tbot"
 )
 
 func ConfigDateHandler(message *tbot.Message) {
-  // store latest message as a global (OMG!)
-  chatId = message.ChatID;
+	// store latest message as a global (OMG!)
+	chatId = message.ChatID
 
-  if message.Vars["date"] == "" {
-    buttons := [][]string{
-      {"/play Monday", "/play Tuesday", },
-      {"/play Wednesday", "/play Thursday"},
-    }
+	if message.Vars["date"] == "" {
+		buttons := [][]string{
+			{"/play Monday", "/play Tuesday"},
+			{"/play Wednesday", "/play Thursday"},
+		}
 
-    message.ReplyKeyboard("What day?", buttons)
+		message.ReplyKeyboard("What day?", buttons)
 
-    return;
-  }
+		return
+	}
 
-  dayOfWeek = message.Vars["date"]
+	dayOfWeek = message.Vars["date"]
 
-  buttons := [][]string{
-    {"/at 17:00", "/at 17:30" },
-    {"/at 18:00", "/at 18:30"},
-  }
+	buttons := [][]string{
+		{"/at 17:00", "/at 17:30"},
+		{"/at 18:00", "/at 18:30"},
+	}
 
-  msg := "Cool! I'll will reserve for " + dayOfWeek + "s. *Do you want to play at "+ hourToPlay +"?*";
-  message.ReplyKeyboard(msg, buttons, tbot.OneTimeKeyboard, tbot.WithMarkdown)
+	msg := "Cool! I'll will reserve for " + dayOfWeek + "s. *Do you want to play at " + hourToPlay + "?*"
+	message.ReplyKeyboard(msg, buttons, tbot.OneTimeKeyboard, tbot.WithMarkdown)
 }
 
 func ConfigTimeHandler(message *tbot.Message) {
-  // store latest message as a global (OMG!)
-  chatId = message.ChatID;
+	// store latest message as a global (OMG!)
+	chatId = message.ChatID
 
-  if message.Vars["time"] == "" {
-    buttons := [][]string{
-      {"/at 17:00", "/at 17:30" },
-      {"/at 18:00", "/at 18:30"},
-    }
+	if message.Vars["time"] == "" {
+		buttons := [][]string{
+			{"/at 17:00", "/at 17:30"},
+			{"/at 18:00", "/at 18:30"},
+		}
 
-    message.ReplyKeyboard("What hour?", buttons, tbot.OneTimeKeyboard)
+		message.ReplyKeyboard("What hour?", buttons, tbot.OneTimeKeyboard)
 
-    return;
-  }
+		return
+	}
 
-  hourToPlay = message.Vars["time"];
+	hourToPlay = message.Vars["time"]
 
-  message.Reply("Cool! Consider it done!")
+	message.Reply("Cool! Consider it done!")
 
-  ConfigShowHandler(message);
+	ConfigShowHandler(message)
 }
 
 func ConfigShowHandler(message *tbot.Message) {
-  // store latest message as a global (OMG!)
-  chatId = message.ChatID;
+	// store latest message as a global (OMG!)
+	chatId = message.ChatID
 
-  ndate := nextDateForSchedule(dayOfWeek, hourToPlay);
-  tdate := ndate.AddDate(0, 0, -7);
+	ndate := nextDateForSchedule(dayOfWeek, hourToPlay)
+	tdate := ndate.AddDate(0, 0, -7)
 
-  msgs := []string {
-    "I'll will reserve for *"+ dayOfWeek +"s at "+ hourToPlay +"*. Next available date for schedule is *"+ ndate.Format("02-01-2006") +"*.",
-    "_And don't worry, I'll automatically perform the reservation on "+ tdate.Format("02-01-2006") +"'s night_",
-  }
+	msgs := []string{
+		"I'll will reserve for *" + dayOfWeek + "s at " + hourToPlay + "*. Next available date for schedule is *" + ndate.Format("02-01-2006") + "*.",
+		"_And don't worry, I'll automatically perform the reservation on " + tdate.Format("02-01-2006") + "'s night_",
+	}
 
-  message.Reply(strings.Join(msgs, "\n"), tbot.WithMarkdown)
+	message.Reply(strings.Join(msgs, "\n"), tbot.WithMarkdown)
 }
